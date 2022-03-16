e004dde63f6a6434bf9ed6b077a15791f27a161b
Kauã Maia Aquino <66255157+NataliaFernan@users.noreply.github.com>
2022-03-16 12:56:28 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer2 } from '@angular/core';
+import { AnimationController, Animation, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('blocks') blocks: any;
+  @ViewChild('background') background: any;
 
-  constructor() {}
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: 'Indicar amigos'},
+    { icon: 'phone-portrait-outline', text: 'Recarga de celular'},
+    { icon: 'wallet-outline', text: 'Depositar'},
+    { icon: 'options-outline', text: 'Ajustar limite'},
+    { icon: 'help-circle-outline', text: 'Me ajuda'},
+    { icon: 'barcode-outline', text: 'Pagar'},
+    { icon: 'lock-open-outline', text: 'Bloquear cartão'},
+    { icon: 'card-outline', text: 'Cartão virtual'},
+  ];
 
+  public slidesOptions: any = { slidesPerView: 3, freeMode: true};
+  public items: Array<any> = [
+    { icon: 'help-circle-outline', text:'Me ajuda'},
+    { icon: 'person-outline', text: 'Perfil'},
+    { icon: 'cash-outline', text: 'Configurar conta'},
+    { icon: 'card-outline', text: 'Configurar cartão'},
+    { icon: 'phone-portrait-outline', text: 'Configurações do app'},
+  ];
+
+ 
 }
